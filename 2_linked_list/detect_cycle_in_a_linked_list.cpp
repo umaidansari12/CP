cc65b46bc1afd545c2a7dd9d07732b7f32b0fa82
umaidansari12 <umaidansari123@gmail.com>
2022-04-16 00:46:40 +0530

    Slow and fast pointer method added

--- 2_linked_list/detect_cycle_in_a_linked_list.cpp
+++ 2_linked_list/detect_cycle_in_a_linked_list.cpp
@@ -13,3 +13,19 @@ bool detectCycle(Node *head)
 	}
 	return false;
 }
+
+bool detectCycle(Node *head)
+{
+	//	Write your code here
+	Node* slow = head;
+	Node* fast = head;
+	// we are checking if fast pointer doesnt becomes null because null indicates that there is no cycle  and if there is a cycle then fast and slow pointer are bound to meet at some point in the cycle
+	while (fast->next and fast) {
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return true;
+	}
+
+	return false;
+}
