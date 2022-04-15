05b53d8c03d63fd6664c5d3160cc7b2c57c5aade
umaidansari12 <umaidansari123@gmail.com>
2022-04-16 01:19:31 +0530

    modified 2_linked_list/reverse_a_linked_list_in_groups_of_size_k.cpp

--- 2_linked_list/reverse_a_linked_list_in_groups_of_size_k.cpp
+++ 2_linked_list/reverse_a_linked_list_in_groups_of_size_k.cpp
@@ -69,6 +69,33 @@ public:
         }
         return prev;
     }
+
+    node* reverseKNodes(node* head, int k) {
+        if (head == NULL || head->next == NULL) return head;
+
+        int length = lengthOfLinkedList(head);
+
+        node* dummyHead = new node(0);
+        dummyHead->next = head;
+
+        node* pre = dummyHead;
+        node* cur;
+        node* nex;
+
+        while (length >= k) {
+            cur = pre->next;
+            nex = cur->next;
+            for (int i = 1; i < k; i++) {
+                cur->next = nex->next;
+                nex->next = pre->next;
+                pre->next = nex;
+                nex = cur->next;
+            }
+            pre = cur;
+            length -= k;
+        }
+        return dummyHead->next;
+    }
 };
 
 
