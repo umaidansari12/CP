7a88f56f12666a6876ca2aeb061b94c28a3e9294
umaidansari12 <umaidansari123@gmail.com>
2022-04-21 23:21:01 +0530

    modified 2_linked_list/flattening_of_a_linked_list.cpp

--- 2_linked_list/flattening_of_a_linked_list.cpp
+++ 2_linked_list/flattening_of_a_linked_list.cpp
@@ -57,3 +57,48 @@ Node* flattenLinkedList(Node* head)
 
 	return head;
 }
+
+
+Node* merge(Node* a, Node* b) {
+	Node* dummy_head = new Node(0);
+	Node* head = dummy_head;
+
+	while (a != NULL and b != NULL) {
+		if (a->data < b->data) {
+			dummy_head->child = a;
+			dummy_head = dummy_head->child;
+			a = a->child;
+		}
+		else {
+			dummy_head->child = b;
+			dummy_head = dummy_head->child;
+			b = b->child;
+		}
+	}
+
+	if (a) {
+		dummy_head->child = a;
+	}
+	else if (b) {
+		dummy_head->child = b;
+	}
+
+	return head->child;
+}
+
+Node* flattenLinkedList(Node* head)
+{
+	// Write your code here
+	if (head == NULL or head->next == NULL) {
+		return head;
+	}
+
+	Node* a = head;
+	Node* answer = NULL;
+	while (a) {
+		answer = merge(answer, a);
+		a = a->next;
+	}
+
+	return answer;
+}
