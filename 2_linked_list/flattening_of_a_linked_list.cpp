8cd323b30a66bc290e2f4dd5eb1903610b401e8e
umaidansari12 <umaidansari123@gmail.com>
2022-04-21 12:44:28 +0530

    modified 2_linked_list/flattening_of_a_linked_list.cpp

---
+++ 2_linked_list/flattening_of_a_linked_list.cpp
@@ -0,0 +1,59 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+class Node {
+public:
+	int data;
+	Node* next;
+	Node* child;
+
+	Node(int data) {
+		this->data = data;
+		this->next = NULL;
+		this->child = NULL;
+	}
+};
+
+*****************************************************************/
+
+Node* merge(Node* a, Node* b) {
+	Node* dummy_head = new Node(0);
+	Node* head = dummy_head;
+
+	while (a != NULL and b != NULL) {
+		if (a->data < b->data) {
+			dummy_head->child = a;
+			dummy_head = dummy_head->child;
+			a = a->child;
+		}
+		else {
+			dummy_head->child = b;
+			dummy_head = dummy_head->child;
+			b = b->child;
+		}
+	}
+
+	if (a) {
+		dummy_head->child = a;
+	}
+	else if (b) {
+		dummy_head->child = b;
+	}
+
+	return head->child;
+}
+
+Node* flattenLinkedList(Node* head)
+{
+	// Write your code here
+	if (head == NULL or head->next == NULL) {
+		return head;
+	}
+
+	head->next = flattenLinkedList(head->next);
+
+	head = merge(head, head->next);
+
+	return head;
+}
