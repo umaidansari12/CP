277f754f32fbb20ee724ed9402b8fe53687c7c39
umaidansari12 <umaidansari123@gmail.com>
2022-04-23 20:08:32 +0530

    added 1_tree/iterative_preorder_traversal.cpp

--- 1_tree/iterative_preorder_traversal.cpp
+++ 1_tree/iterative_preorder_traversal.cpp
@@ -149,3 +149,57 @@ vector <int> preorder(Node* root)
     return ans;
 
 }
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> recursive_stack;
+        vector<int> answer;
+        while (true) {
+            if (root != NULL) {
+                answer.push_back(root->val);
+                recursive_stack.push(root);
+                root = root->left;
+            }
+            else {
+                if (recursive_stack.empty()) {
+                    break;
+                }
+
+                root = recursive_stack.top();
+                recursive_stack.pop();
+
+                root = root->right;
+
+            }
+        }
+        return answer;
+    }
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> answer;
+        if (root == NULL)
+            return answer;
+
+        stack<TreeNode*> recursive_stack;
+
+        recursive_stack.push(root);
+
+        while (!recursive_stack.empty()) {
+            TreeNode* curr = recursive_stack.top();
+            recursive_stack.pop();
+
+            answer.push_back(curr->val);
+
+            if (curr->right != NULL)
+                recursive_stack.push(curr->right);
+            if (curr->left != NULL)
+                recursive_stack.push(curr->left);
+        }
+
+        return answer;
+    }
+};
