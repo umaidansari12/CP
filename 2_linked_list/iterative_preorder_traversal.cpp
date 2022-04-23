e8b15286c7912b0b669865892f72d5f5d880fcf3
umaidansari12 <umaidansari123@gmail.com>
2022-04-23 20:09:19 +0530

    modified 2_linked_list/iterative_preorder_traversal.cpp

--- 2_linked_list/iterative_preorder_traversal.cpp
+++ 2_linked_list/iterative_preorder_traversal.cpp
@@ -24,3 +24,30 @@ public:
         return answer;
     }
 };
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> answer;
+        if (root == NULL)
+            return answer;
+
+        stack<TreeNode*> recursive_stack;
+
+        recursive_stack.push(root);
+
+        while (!recursive_stack.empty()) {
+            TreeNode* curr = recursive_stack.top();
+            recursive_stack.pop();
+
+            answer.push_back(curr->val);
+
+            if (curr->right != NULL)
+                recursive_stack.push(curr->right);
+            if (curr->left != NULL)
+                recursive_stack.push(curr->left);
+        }
+
+        return answer;
+    }
+};
