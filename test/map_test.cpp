8ed7eed2abcd189e24752f6d764a44374900c50b
umaidansari12 <umaidansari123@gmail.com>
2022-04-24 19:59:51 +0530

    added test/map_test.cpp

---
+++ test/map_test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	map<pair<int, int>, vector<int>> ds;
+
+	ds[1][2].push_back(3);
+}
